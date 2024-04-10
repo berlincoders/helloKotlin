eb018041855d4399f400daa7237c77b0c77823d0
Romeo <ralonsoad@gmail.com>
2024-04-10 18:39:49 +0200

    play with variables

--- src/Main.kt
+++ src/Main.kt
@@ -1,3 +1,5 @@
+import java.util.*
+
 fun main() {
     println("Hello Kotlin!")
 
@@ -13,10 +15,20 @@ fun main() {
     // is like final variable and it's known as immutable in Kotlin
     // and can be initialized only single time
 
-    val num : Int= 3
+    var num : Int= 3
     println("the value of num is $num")
     num = 4
     println("The value of num is now $num ")
+
+    //Arithmetic Operations
+    val x = 3 * 4
+    val y = 3f / 4f  // float values
+    val z = 10 % 3   // rest
+    println("The value of x is $x  and y is $y  and z $z")
+
+    //strings
+    val string =" kotlin is a cool language "
+    println(string.uppercase()) /* toUpperCase() is deprecated. use uppercase() , the most important is to learn the shortcut (Option+ Caps + Enter) */
 
 
 }

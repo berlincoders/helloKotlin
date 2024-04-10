d5b1dae5613a09fe1617276cb966789ddf5acfa8
Romeo <ralonsoad@gmail.com>
2024-04-10 17:15:40 +0200

    Hello Kotlin

--- src/Main.kt
+++ src/Main.kt
@@ -1,3 +1,22 @@
 fun main() {
-    println("Hello World!")
+    println("Hello Kotlin!")
+
+    // Interpolation in kotlin  $num
+    //       "       javascript ${num}
+    //       "       ruby       #{num}
+    //       "       Python      {num}
+
+    // var:
+    //Is like general variable and it's known as a mutable variable in Kotlin
+    // and can be assigned multiple times.
+    // val:
+    // is like final variable and it's known as immutable in Kotlin
+    // and can be initialized only single time
+
+    val num : Int= 3
+    println("the value of num is $num")
+    num = 4
+    println("The value of num is now $num ")
+
+
 }
